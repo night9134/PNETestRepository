907461f8696c983ae9094571d9312ae1e3f31ab5
Donghun Kim <dhkim82@pnesolution.com>
2021-03-22 19:37:16 +0900

    main.cpp 추가
    12가지 수학 함수 추가
    해당 기능 개발과 함께 브랜치와 머지 등 교육 목적

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	while (true)
+	{
+		int nInputManu = 0;
+
+		cout << '\n'
+			 << "Select Method below";
+		cout << '\n'
+			 << "1. Swap two number";
+		cout << '\n'
+			 << "2. Quotient and Remainder";
+		cout << '\n'
+			 << "3. Check whether number is Even or Odd";
+		cout << '\n'
+			 << "4. Calculate Sum of Natural Numbers";
+		cout << '\n'
+			 << "5. Check Leap Year";
+		cout << '\n'
+			 << "6. Factorial";
+		cout << '\n'
+			 << "7. Find Largest element of an array";
+		cout << '\n'
+			 << "8. Check Prime Number";
+		cout << '\n'
+			 << "9. Display Prime Number between two intervals";
+		cout << '\n'
+			 << "10. Calculate Power of a Number";
+		cout << '\n'
+			 << "11. Find GCD";
+		cout << '\n'
+			 << "12. Find LCM";
+		cout << '\n'
+			 << "Select Number>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>";
+	}
+
+	return 0;
+}
