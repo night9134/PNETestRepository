97a2a501901987eaefdef492da86b79a839ab18b
Donghun Kim <dhkim82@pnesolution.com>
2021-03-23 09:41:35 +0900

    담당자 이름 지정
    			 << "1. Swap two number 황현성";
    		cout << '\n'
    			 << "2. Quotient and Remainder 박재성";
    		cout << '\n'
    			 << "3. Check whether number is Even or Odd 전성호";
    		cout << '\n'
    			 << "4. Calculate Sum of Natural Numbers 김은경";
    		cout << '\n'
    			 << "5. Check Leap Year 김량훈";
    		cout << '\n'
    			 << "6. Factorial 김주성";
    		cout << '\n'
    			 << "7. Find Largest element of an array 김주성";
    		cout << '\n'
    			 << "8. Check Prime Number 김량훈";
    		cout << '\n'
    			 << "9. Display Prime Number between two intervals 김은경";
    		cout << '\n'
    			 << "10. Calculate Power of a Number 전성호";
    		cout << '\n'
    			 << "11. Find GCD 박재성";
    		cout << '\n'
    			 << "12. Find LCM 황현성";

--- main.cpp
+++ main.cpp
@@ -10,29 +10,29 @@ int main()
 		cout << '\n'
 			 << "Select Method below";
 		cout << '\n'
-			 << "1. Swap two number";
+			 << "1. Swap two number 황현성";
 		cout << '\n'
-			 << "2. Quotient and Remainder";
+			 << "2. Quotient and Remainder 박재성";
 		cout << '\n'
-			 << "3. Check whether number is Even or Odd";
+			 << "3. Check whether number is Even or Odd 전성호";
 		cout << '\n'
-			 << "4. Calculate Sum of Natural Numbers";
+			 << "4. Calculate Sum of Natural Numbers 김은경";
 		cout << '\n'
-			 << "5. Check Leap Year";
+			 << "5. Check Leap Year 김량훈";
 		cout << '\n'
-			 << "6. Factorial";
+			 << "6. Factorial 김주성";
 		cout << '\n'
-			 << "7. Find Largest element of an array";
+			 << "7. Find Largest element of an array 김주성";
 		cout << '\n'
-			 << "8. Check Prime Number";
+			 << "8. Check Prime Number 김량훈";
 		cout << '\n'
-			 << "9. Display Prime Number between two intervals";
+			 << "9. Display Prime Number between two intervals 김은경";
 		cout << '\n'
-			 << "10. Calculate Power of a Number";
+			 << "10. Calculate Power of a Number 전성호";
 		cout << '\n'
-			 << "11. Find GCD";
+			 << "11. Find GCD 박재성";
 		cout << '\n'
-			 << "12. Find LCM";
+			 << "12. Find LCM 황현성";
 		cout << '\n'
 			 << "Select Number>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>";
 	}
